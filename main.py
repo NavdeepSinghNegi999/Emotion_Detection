6979074d833c77e51613251cae4d2870ea5e1211
Navdeep Singh <132869610+NavdeepSinghNegi999@users.noreply.github.com>
2024-05-20 21:52:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import cv2
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, VideoProcessorBase, WebRtcMode
 
 
-# st.set_option('deprecation.showfileUploaderEncoding', False)
-# showfileUploaderEncoding = False
+st.set_option('deprecation.showfileUploaderEncoding', False)
+showfileUploaderEncoding = False
 
 @st.cache(allow_output_mutation=True)
 def load_my_model():
@@ -37,8 +37,6 @@ def model_prediction(test_image):
     
     return result_index
 
-
-
 RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
 
 try:
@@ -46,40 +44,11 @@ try:
 except Exception:
     st.write("Error loading cascade classifiers")
 
-class FaceEmotion(VideoTransformerBase):
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-        
-        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        
-        faces = face_cascade.detectMultiScale(image=img_rgb, scaleFactor=1.3, minNeighbors=5)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img=img, pt1=(x, y), pt2=(x+w, y+h), color=(255, 0, 0), thickness=2)
-            
-            roi = img[y:y + h, x:x + w]
-            
-            
-            roi_gray = cv2.resize(roi, (150, 150), interpolation=cv2.INTER_AREA)
-            if np.sum([roi_gray]) != 0:
-                roi = roi_gray.astype('float') / 255.0
-                roi = tf.keras.preprocessing.image.img_to_array(roi)
-                roi = np.expand_dims(roi, axis=0)
-                
-                # prediction = model.predict(roi)[0]
-                # result_index = int(np.argmax(prediction))
-                # output = index_to_emotion(result_index)
-                output = "happy"
-                
-                
-            
-            label_position = (x, y)
-            cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-        return img
 
 #sidebar
 st.sidebar.title("Dashboard")
-app_mode = st.sidebar.selectbox("Select Page", ["Image Emotion Prediction", "Live emotion Prediction"])
+app_mode = st.sidebar.selectbox("Select Page", ["Image Emotion Prediction"])
 
 # Home Page
 if(app_mode == "Image Emotion Prediction"):
@@ -97,14 +66,7 @@ if(app_mode == "Image Emotion Prediction"):
         result = index_to_emotion(result_index)
         st.success(f"{result}")
         
-        
-if(app_mode == "Live emotion Prediction"):
-    st.header("Emotion Detection")
-    
-    
-    st.title("Live Video Feed")
-    webrtc_streamer(key="example", mode = WebRtcMode.SENDRECV, rtc_configuration=RTC_CONFIGURATION,
-                        video_processor_factory = FaceEmotion)
+
     
     
     
