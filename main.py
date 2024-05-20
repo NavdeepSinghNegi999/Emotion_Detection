ccc6da7448ee1c6e610775fb61bd55b96a37c5ad
Navdeep Singh <132869610+NavdeepSinghNegi999@users.noreply.github.com>
2024-05-20 17:31:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import cv2
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, VideoProcessorBase, WebRtcMode
 
 
-
+# st.set_option('deprecation.showfileUploaderEncoding', False)
+# showfileUploaderEncoding = False
 
 @st.cache(allow_output_mutation=True)
 def load_my_model():
